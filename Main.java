016eb2b579df72c68b5e87fa25cb39886d67396d
adityatustanda <adityatustanda28@gmail.com>
2023-04-18 15:20:24 +0800

    Upload Restaurant,Main,Admin,Login

---
+++ Main.java
@@ -0,0 +1,25 @@
+package TugasPBO;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("LOGIN");
+        System.out.println("1.Admin");
+        System.out.println("2.Customer");
+        System.out.println("Silakan pilih jenis pengguna:");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Anda masuk sebagai Admin.");
+        } else if (choice == 2) {
+            System.out.println("Anda masuk sebagai Customer.");
+        } else {
+            System.out.println("Pilihan tidak valid.");
+        }
+
+        scanner.close();
+    }
+}
