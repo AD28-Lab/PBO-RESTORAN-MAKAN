dc0f17d403ab3b248abf8f7f18de394bed9e4871
AdityaTustanda28 <115051686+AD28-Lab@users.noreply.github.com>
2023-07-12 23:13:50 +0800

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,19 @@
+
+public class Menu {
+    private String nama;
+    private int harga;
+
+    public Menu(String name, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getName() {
+        return nama;
+    }
+
+    public int getPrice() {
+        return harga;
+    }
+}
+
